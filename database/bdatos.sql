ee041d5abe96c86e407a8edc40c7a1273111b1af
washington <washington_nieto@hotmail.com>
2025-08-29 20:35:41 -0500

    actualizacion tabla productos

--- database/bdatos.sql
+++ database/bdatos.sql
@@ -10,10 +10,31 @@ CREATE TABLE IF NOT EXISTS t_usuarios(
     password    varchar(200)                NOT NULL,
     rol         varchar(20)                 NOT NULL,
     imagen      varchar(200),
-    CONSTRAINT  pk_usuario      PRIMARY KEY(id_usuario),
-    CONSTRAINT  uq_email        UNIQUE(email)
+    CONSTRAINT  pk_usuario                  PRIMARY KEY(id_usuario),
+    CONSTRAINT  uq_email                    UNIQUE(email)
 )ENGINE=InnoDb;
 
 INSERT INTO t_usuarios VALUE(NULL, 'Washington','Nieto','wnieto@gmail.com','123456789','admin','imagen');
 
+CREATE TABLE IF NOT EXISTS t_categorias(
+    id_categoria        int(11) auto_increment  NOT NULL,
+    nombre_categoria    varchar(100)            NOT NULL,
+    CONSTRAINT          pk_categoria            PRIMARY KEY(id_categoria)
+)ENGINE=InnoDb;
 
+INSERT INTO t_categorias VALUE(NULL, 'Contruccion'),(NULL, 'Electricos'),(Null, 'Pinturas'),(NULL, 'Pisos');
+
+
+CREATE TABLE IF NOT EXISTS t_productos(
+    id_producto     int(11) auto_increment  NOT NULL,
+    nombre_producto varchar(100)            NOT NULL,
+    descripcion     varchar(200)            NOT NULL,
+    precio          float(10,2)             NOT NULL,
+    stock           int(100)                NOT NULL,
+    oferta          varchar(50)             NOT NULL,
+    fecha           date                    NOT NULL,
+    imagen          varchar(255)            NOT NULL,
+    id_categoria    int(11)                 NOT NULL,
+    CONSTRAINT      pk_producto             PRIMARY KEY(id_producto),
+    CONSTRAINT      fk_producto_categoria   FOREIGN KEY(id_categoria) REFERENCES t_categorias(id_categoria)
+)ENGINE=InnoDb;
